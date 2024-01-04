02330ba3b50313e2c11cf106ce2985a716aaf664
Royce <royce236@gmail.com>
2024-01-05 00:33:50 +0800

    Transfer values to constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,7 @@
+// time constants
+export const DEFAULT_WORK_MODE_MINUTES = 25;
+export const DEFAULT_BREAK_MODE_MINUTES = 5;
+
+// display text constants
+export const DEFAULT_WORK_MODE_MESSAGE = 'Work mode';
+export const DEFAULT_BREAK_MODE_MESSAGE = 'Take a break';
