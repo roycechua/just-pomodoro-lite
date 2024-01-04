7299d411f696e642a3d07c2d41bbc9bc7593888c
Royce <royce236@gmail.com>
2024-01-04 23:55:56 +0800

    Implement basic toggling logic between work mode and break mode

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,78 +1,20 @@
 // import { useGlobalAppState } from '../providers/GlobalAppStateProvider';
-import { useCallback, useEffect } from 'react';
-import useTimer from '../hooks/useTimer';
-import { FaPlay } from 'react-icons/fa';
-import { FaPause } from 'react-icons/fa';
+
+import { useState } from 'react';
+import CountdownTimer from './CountdownTimer';
 
 export default function Countdown() {
   // const [globalAppState, dispatch] = useGlobalAppState();
-  const { hours, minutes, seconds, isTimerRunning, start, stop } = useTimer({
-    minutes: 5,
-  });
-
-  const handleTimerButtonPressed = useCallback(() => {
-    if (isTimerRunning) {
-      stop();
-    } else {
-      start();
-    }
-  }, [isTimerRunning, start, stop]);
-
-  useEffect(() => {
-    const keyListener = (e: KeyboardEvent) => {
-      // if spacebar was presed
-      if (e.key === ' ') {
-        handleTimerButtonPressed();
-      }
-    };
-    window.addEventListener('keypress', keyListener);
-    return () => {
-      window.removeEventListener('keypress', keyListener);
-    };
-  }, [isTimerRunning, handleTimerButtonPressed]);
-
-  const renderTimerButtonLabel = isTimerRunning ? (
-    <>
-      <FaPause color={'#9EE493'} size={45} style={{ marginRight: 10 }} />
-      <p style={{ margin: 0, fontSize: '3rem' }}>Stop</p>
-    </>
-  ) : (
-    <>
-      <FaPlay color={'#9EE493'} size={45} style={{ marginRight: 10 }} />
-      <p style={{ margin: 0, fontSize: '3rem' }}>Play</p>
-    </>
-  );
-
+  const [workMode, setWorkMode] = useState(true);
   return (
-    <div
-      style={{
-        display: 'flex',
-        flex: 1,
-        alignItems: 'center',
-        flexDirection: 'column',
-      }}
-    >
-      <p className='timer'>
-        {hours > 0 ? `${hours.toString().padStart(2, '0')}:` : null}
-        {minutes.toString().padStart(2, '0')}:
-        {seconds.toString().padStart(2, '0')}
-      </p>
-
-      <div
-        style={{
-          padding: 20,
-          borderRadius: 10,
-          fontSize: 30,
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexDirection: 'row',
-          display: 'flex',
-          cursor: 'pointer',
+    <>
+      <CountdownTimer
+        initialMinutes={workMode ? 25 : 5}
+        message={workMode ? 'Work mode' : 'Take a break'}
+        onComplete={() => {
+          setWorkMode((prevValue) => !prevValue);
         }}
-        onClick={handleTimerButtonPressed}
-      >
-        {renderTimerButtonLabel}
-      </div>
-    </div>
+      />
+    </>
   );
 }
