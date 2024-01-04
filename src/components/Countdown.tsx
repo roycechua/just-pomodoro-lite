dbb7a9af1116951a72cf618898df32043d50b9f4
Royce <royce236@gmail.com>
2024-01-05 00:28:12 +0800

    Add logic to play sound after every cycle

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,18 +1,31 @@
 // import { useGlobalAppState } from '../providers/GlobalAppStateProvider';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import CountdownTimer from './CountdownTimer';
+import useAudio from '../hooks/useAudio';
 
 export default function Countdown() {
   // const [globalAppState, dispatch] = useGlobalAppState();
   const [workMode, setWorkMode] = useState(true);
+  const { play: playSound, stop: stopSound } = useAudio(
+    '/break_alarm.mp3',
+    false
+  );
+
+  useEffect(() => {
+    playSound();
+    setTimeout(() => {
+      stopSound();
+    }, 10000);
+  }, [workMode]);
+
   return (
     <>
       <CountdownTimer
         initialMinutes={workMode ? 25 : 5}
         message={workMode ? 'Work mode' : 'Take a break'}
         onComplete={() => {
-          setWorkMode((prevValue) => !prevValue);
+          setWorkMode(!workMode);
         }}
       />
     </>
