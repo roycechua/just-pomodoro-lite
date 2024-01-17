e3490eb5607e735b3b59ba43ea3d042b481c5993
Royce <royce236@gmail.com>
2024-01-18 01:00:05 +0800

    Update mode handling

--- src/components/Pomodoro.tsx
+++ src/components/Pomodoro.tsx
@@ -9,33 +9,40 @@ import {
   DEFAULT_WORK_MODE_MESSAGE,
   DEFAULT_WORK_MODE_MINUTES,
 } from '../utils/constants';
+import { ModeType } from '../types';
 
 export default function Countdown() {
   // const [globalAppState, dispatch] = useGlobalAppState();
-  const [workMode, setWorkMode] = useState(true);
+  const [mode, setMode] = useState<ModeType>('work');
   const { play: playSound, stop: stopSound } = useAudio(
     'alarms/break_alarm2.mp3',
     false
   );
 
+  // play sound on every mode change
   useEffect(() => {
     playSound();
     setTimeout(() => {
       stopSound();
     }, 10000);
-  }, [workMode]);
+  }, [mode]);
 
   return (
     <>
       <CountdownTimer
         initialMinutes={
-          workMode ? DEFAULT_WORK_MODE_MINUTES : DEFAULT_BREAK_MODE_MINUTES
+          mode === 'work'
+            ? DEFAULT_WORK_MODE_MINUTES
+            : DEFAULT_BREAK_MODE_MINUTES
         }
         message={
-          workMode ? DEFAULT_WORK_MODE_MESSAGE : DEFAULT_BREAK_MODE_MESSAGE
+          mode === 'work'
+            ? DEFAULT_WORK_MODE_MESSAGE
+            : DEFAULT_BREAK_MODE_MESSAGE
         }
         onComplete={() => {
-          setWorkMode(!workMode);
+          const newMode = mode === 'work' ? 'break' : 'work';
+          setMode(newMode);
         }}
       />
     </>
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -26,3 +26,5 @@ export interface TimerInputType {
   seconds?: number;
   onComplete?: () => void;
 }
+
+export type ModeType = 'work' | 'break';
