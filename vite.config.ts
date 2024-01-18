94ae9d2ada018d4bc235afa30e2c5dbf89937984
Royce <royce236@gmail.com>
2024-01-18 22:14:34 +0800

    Add PWA items

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,37 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      includeAssets: [],
+      manifest: {
+        name: 'Just Pomodoro Lite',
+        short_name: 'JustPomodoro',
+        description: 'Just a simple pomodoro timer app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
       devOptions: {
         enabled: true,
       },
